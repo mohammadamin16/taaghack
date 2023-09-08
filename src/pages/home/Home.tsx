0e679a7662ff2f9bbef6b9f3e50992bf605392c8
Amin Tootoonchi <toutounchi.ma@gmail.com>
2023-09-08 18:45:16 +0330

    feat: Feature 5 ✅

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -10,45 +10,73 @@ import {
 } from "@mui/material";
 import { useEffect, useMemo, useState } from "react";
 
+enum SortOptions {
+  MostExpensive = "بیشترین قیمت",
+  LeastExpensive = "کمترین قیمت",
+  MostPopular = "بیشترین امتیاز",
+  LessPopular = "کمترین امتیاز",
+}
 export const Home = () => {
   const { data, loading } = useTaaghche();
   const [selectedPublisher, setSelectedPublisher] = useState("");
+  const [sorter, setSorter] = useState<SortOptions>(SortOptions.MostPopular);
 
   const publishers = useMemo<string[]>(() => {
     if (!data) return [];
     return data.bookList.books.map((b) => b.publisher);
   }, [data]);
 
+  const handleSortOptionChange = (event: SelectChangeEvent) => {
+    setSorter((event.target.value as unknown) as SortOptions);
+  };
+
   const handleChange = (event: SelectChangeEvent) => {
     setSelectedPublisher(event.target.value);
   };
 
-  useEffect(() => {
-    console.log("selectedPublisher", selectedPublisher);
-  }, [selectedPublisher]);
-
   return (
     <div className={styles.home}>
-      <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel id="demo-simple-select-standard-label">
-          Publisher
-        </InputLabel>
-        <Select
-          labelId="demo-simple-select-standard-label"
-          id="demo-simple-select-standard"
-          value={selectedPublisher}
-          onChange={handleChange}
-          label="Age"
-        >
-          <MenuItem value="">No Filter!</MenuItem>
-          {publishers.map((p) => (
-            <MenuItem value={p}>{p}</MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <div className={styles.filter_row}>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-standard-label">قیمت</InputLabel>
+          <Select
+            value={sorter}
+            onChange={handleSortOptionChange}
+            label="Filter"
+          >
+            <MenuItem value={SortOptions.MostPopular}>بیشترین امتیاز</MenuItem>
+            <MenuItem value={SortOptions.LeastExpensive}>کمترین قیمت</MenuItem>
+            <MenuItem value={SortOptions.MostExpensive}>بیشترین قیمت</MenuItem>
+            <MenuItem value={SortOptions.LessPopular}>کمترین امتیاز</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+          <InputLabel id="demo-simple-select-standard-label">
+            Publisher
+          </InputLabel>
+          <Select value={selectedPublisher} onChange={handleChange} label="Age">
+            <MenuItem value="">No Filter!</MenuItem>
+            {publishers.map((p) => (
+              <MenuItem value={p}>{p}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
       <div className={styles.book_list}>
         {loading && <p>Loading...</p>}
         {data?.bookList.books
+          .sort((b1, b2) => {
+            switch (sorter) {
+              case SortOptions.LeastExpensive:
+                return b1.price - b2.price < 0 ? -1 : 1;
+              case SortOptions.LessPopular:
+                return b1.rating - b2.rating < 0 ? -1 : 1;
+              case SortOptions.MostExpensive:
+                return b1.price - b2.price < 0 ? 1 : -1;
+              case SortOptions.MostPopular:
+                return b1.rating - b2.rating < 0 ? 1 : -1;
+            }
+          })
           .filter((b) => {
             if (selectedPublisher) {
               return b.publisher === selectedPublisher;
